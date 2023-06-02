1bf0e1840d45790ae3a48c800227821377c76dd8
irmafrat <irmafraticelli412@gmail.com>
2023-06-02 17:23:05 -0400

    End of the Day

--- learning_eutils.py
+++ learning_eutils.py
@@ -1,30 +1,79 @@
+#https://pypi.org/project/nbib/
+#PubmedArticleSet https://dtd.nlm.nih.gov/ncbi/pubmed/el-PubmedArticleSet.html
+#From nbib to RIS?? A bash tool??
+
 import eutils
-ec = eutils.Client()
+from eutils._internal.xmlfacades.pubmedarticle import PubmedArticle
+from eutils._internal.xmlfacades.pubmedarticleset import PubmedArticleSet
+import csv
+
+ec = eutils.Client()    
+
+#SUBSTITUTE
+csvfile = "pmid_list.csv"
+
+#Get IDs from a csv
+pmid_list= list()
+with open(csvfile) as csv_file:
+    csv_reader= csv.DictReader(csv_file, delimiter=",")
+    for row in csv_reader:
+        pmid=row['pmid']
+        if pmid == "NA" or None:
+            continue
+        pmid_list.append(pmid)
+            
+        #print(pmid)
 
 # search for pcos in pubmed
 # any valid NCBI query may be used
-query = ec.esearch(db='pubmed',term='pcos[all]')
+#query = ec.esearch(db='pubmed',term=pmid)
 #print(f"here is your query:\n{query}")
 #print(f"here is your list of ids:\n{query.ids}")
 
-#fetch all the records
-fetch = ec.efetch(db="pubmed",id= query.ids)
-#print(f"here is what you fetch:\n{fetch}")
+#fetch all the records. Collection of Pubmed Articles
+article_set = ec.efetch(db="pubmed",id=pmid_list)
+print(article_set)
+# articles = next(iter(fetch))
+# print(f"here is what you fetch:\n{articles}")
 
 #Iterate over a collection of pubmed articles
 #Receives a eutils._internal.xmlfacades.PubmedArticleSet
-#Returns str
 
-for article in fetch:
-    authors = article.authors
-    title = article.title
-    journal = article.jrnl
-    issue =article.issue
-    year = article.year
-    pages = article.pages
-    doi = article.doi
-    pmc= article.pmc
-    pmid = article.pmid
-    print(f"\n{authors}.{title},{journal},{issue},DOI:{doi},PMCID:{pmc},PMID:{pmid}")
+
+def articleRIS(article: PubmedArticle):
+    all_authors=""
+    for author in article.authors:
+        all_authors = all_authors + f"AU  - {author}\n"
+    all_authors = all_authors.strip("\n")
+    all_pages=""
+    if article.pages:
+        sp = article.pages.split('-')[0]
+        ep = article.pages.split('-')[-1]
+        all_pages = f"SP  - {sp}\nEP  - {ep}"
+    output = f"""TY  - JOUR
+TI  - {article.title}
+{all_authors}
+PY  - {article.year}
+T2  - {article.jrnl}
+VL  - {article.volume}
+IS  - {article.issue}
+{all_pages}
+DO  - {article.doi}
+ID  - {article.pmc}
+NT  - PMCID:{article.pmid}
+AB  - {article.abstract}
+UR  - https://www.ncbi.nlm.nih.gov/pubmed/{article.pmid}
+ER  - 
+""".replace("\n\n","\n")
+    return output
+
+
+def articleSetRIS(aSet: PubmedArticleSet):
+    output = list()
+    for article in aSet:
+        output.append(articleRIS(article))
+    return output
+
+print(articleSetRIS(article_set))
 
 
